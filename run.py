43849a83eebfc3cc60bb401d728dbd8c7eb9dbc6
Robert Hardy <robbert@gmail.com>
2016-10-11 19:26:22 +0000

    Add GET endpoint that returns JSON.

--- run.py
+++ run.py
@@ -1,4 +1,7 @@
-from flask import render_template
+from flask import (
+    jsonify,
+    render_template
+)
 
 
 from app.db import connect_db
@@ -22,5 +25,18 @@ def homepage():
         rows=rows
     )
 
+
+@app.route('/todos')
+def get_todos():
+    conn = connect_db()
+    cur = conn.cursor()
+    cur.execute("""
+        SELECT
+            *
+        FROM
+            todo
+    """)
+    rows = cur.fetchall()
+    return jsonify(rows)
 
 app.run(host='0.0.0.0', port=8080)
